e8d3dd0d2c47557787b8709b6b2de69a4891535c
estarlings <90728491+estarlings@users.noreply.github.com>
2021-10-07 19:49:56 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,43 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+function randomTime(min, max) {
+	return Math.round(Math.random() * (max- min) + min);
+}
+function randomHole(holes){
+	const index = Math.floor(Math.random() * holes.length);
+	const hole = holes[index];
+	
+	if(hole === lastHole) {console.log("That's the same number");
+						   return randomHole(holes);}
+	lastHole = hole;
+	return hole;
+	
+}
+ function peep() {
+	 const time = randomTime(200, 1000);
+	 const hole = randomHole(holes);
+	 hole.classList.add('up');
+	 setTimeout(() => {
+    hole.classList.remove('up');
+    if(!timeUp) peep();
+	 }, time);
+ }
+
+function startGame() {scoreBoard.textContent = 0;
+					 timeUp = false;
+					 score = 0;
+					 peep();
+					 setTimeout(() => timeUp = true, 10000)}
+function wack(e) {if(!e.isTrusted) return;
+				 score++;
+				 this.parentNode.classList.remove('up');
+				 scoreBoard.textContent = score;}
+
+moles.forEach(mole=> mole.addEventListener('click', wack));
+
+
+
+
+
